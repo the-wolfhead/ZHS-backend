7086436c01bcb6f268505bba9f99ca744abd1c6e
Daniel Otabor <danieldamianotabor@gmail.com>
2026-04-20 16:34:24 +0100

    Update withdrawal.routes.js

--- pay/routes/withdrawal.routes.js
+++ pay/routes/withdrawal.routes.js
@@ -1,11 +1,8 @@
-// src/routes/withdrawal.routes.js
-
-import express from "express";
-import { withdraw } from "../controllers/withdrawal.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
+const express = require('express');
+const { withdraw } = require('../controllers/withdrawal.controller.js');
+const { authMiddleware } = require('../middleware/auth.middleware.js');
 
 const router = express.Router();
+router.post('/', authMiddleware, withdraw);
 
-router.post("/", authMiddleware, withdraw);
-
-export default router;
+module.exports = router;

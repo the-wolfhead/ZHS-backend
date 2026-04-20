d2885e79abf876ea7e31f09f0f099e30d74a4ecc
Daniel Otabor <danieldamianotabor@gmail.com>
2026-04-20 16:34:55 +0100

    Update escrow.routes.js

--- pay/routes/escrow.routes.js
+++ pay/routes/escrow.routes.js
@@ -1,17 +1,11 @@
-// src/routes/escrow.routes.js
-
-import express from "express";
-import { authMiddleware } from "../middleware/auth.middleware.js";
-import {
-  create,
-  release,
-  cancel,
-} from "../controllers/escrow.controller.js";
+const express = require('express');
+const { authMiddleware } = require('../middleware/auth.middleware.js');
+const { create, release, cancel } = require('../controllers/escrow.controller.js');
 
 const router = express.Router();
 
-router.post("/create", authMiddleware, create);
-router.post("/release", authMiddleware, release);
-router.post("/cancel", authMiddleware, cancel);
+router.post('/create', authMiddleware, create);
+router.post('/release', authMiddleware, release);
+router.post('/cancel', authMiddleware, cancel);
 
-export default router;
+module.exports = router;

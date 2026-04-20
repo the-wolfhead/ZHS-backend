e6c3fccd82cb4098164e28ef947ed62f8681d3a1
Daniel Otabor <danieldamianotabor@gmail.com>
2026-04-20 16:33:52 +0100

    Update transfer.routes.js

--- pay/routes/transfer.routes.js
+++ pay/routes/transfer.routes.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import { transfer } from '../controllers/transfer.controller.js';
-import { authMiddleware } from '../middleware/auth.middleware.js';
+const express = require('express');
+const { transfer } = require('../controllers/transfer.controller.js');
+const { authMiddleware } = require('../middleware/auth.middleware.js');
 
 const router = express.Router();
+
 router.post('/', authMiddleware, transfer);
-export default router;
+
+module.exports = router;
